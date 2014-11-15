bc544ad27013f1546c7d151305d7da24f9147c1b
Toby Woller <tobywoller@gmail.com>
2014-11-14 16:37:19 -0800

    Initial Classes

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -1,7 +1,34 @@
-class Blackjack
+class Deck
   def initialize
   end
+end
+
+class Hand
+  def initialize
+  end
+end
+
+class Player
+  def initialize
+  end
+end
+
+class Dealer
+  def initialize
+  end
+end
+
+class Blackjack
+  def initialize
+    @deck = Deck.new
+    @hand = Hand.new
+    @player = Player.new
+    @dealer = Dealer.new
+  end
+
   def play
   end
 end
+
+Blackjack.new.play
 
